caf4b6ffd388914f5f12c9188a131df9ef994284
Ben Bryant <brbryant2639@gmail.com>
2022-11-15 12:56:36 -0600

    UPDATE: added test html and js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# puckjsplayground
+# PuckJS Playground
+
+Thanks to Cameron Hardy for the Puck.js gift that prompted this fun!
+
+## Sources
+
+Started from [Espruino's Web Bluetooth Tutorial](https://www.espruino.com/Web+Bluetooth)
---
+++ js/test.js
@@ -0,0 +1,10 @@
+let onBtn = document.querySelector("onButton");
+let offBtn = document.querySelector("offButton");
+
+onBtn.onclick = function () {
+    Puck.write("LED1.set();\n");
+}
+
+offBtn.onclick = function () {
+    Puck.write("LED1.reset();\n");
+}
